bd79c54acffce42555799aa0a085d81a0ea7c5be
Faheem Qazi <sirfaheem@gmail.com>
2026-02-17 23:03:22 +0500

    2026-02-18 start changes for P4

--- anyVideo.py
+++ anyVideo.py
@@ -1,12 +1,13 @@
-import requests
+from pylance import requests
 import subprocess
 
 # Download the video data
 response = requests.get('https://www.youtube.com/watch?v=XGn5cZF-Ogk')
 
 # Save the video data to a file
-with open('/path/to/save/video.mp4', 'wb') as f:
+with open('!video.mp4', 'wb') as f:
     f.write(response.content)
 
 # Use ffmpeg to convert the video to another format (if desired)
-subprocess.run(['ffmpeg', '-i', '/path/to/save/video.mp4', '/path/to/save/video.mkv'])
+#subprocess.run(['ffmpeg', '-i', '!video.mp4', '/path/to/save/video.mkv'])
+#https://www.youtube.com/watch?v=TeIRe4n6MZY
